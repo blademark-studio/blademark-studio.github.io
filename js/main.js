831df20c357f12e3f6231a9dc10949739095e8a7
blademark-studio <blademark.co@gmail.com>
2023-06-08 11:57:22 +0200

    add

--- js/main.js
+++ js/main.js
@@ -435,3 +435,10 @@ document.addEventListener("DOMContentLoaded", function() {
     });
   });
   
+  document.addEventListener("DOMContentLoaded", function() {
+    var images = document.getElementsByTagName("img");
+  
+    for (var i = 0; i < images.length; i++) {
+      images[i].setAttribute("draggable", "false");
+    }
+  });
