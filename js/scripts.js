b7cf034a6fbdaa65ae4c40d15f8cb08bb48df2cc
blademark-studio <blademark.co@gmail.com>
2023-10-26 23:23:22 +0200

    add

--- js/scripts.js
+++ js/scripts.js
@@ -1110,6 +1110,6 @@ document.addEventListener('gesturestart', function (e) {
 	e.preventDefault();
 });
 
-    document.getElementById("po-01").onclick = function () {
-        location.href = "/01.html";
-    };
+    // document.getElementById("po-01").onclick = function () {
+    //     location.href = "/01.html";
+    // };
